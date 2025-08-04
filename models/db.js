18928f94cd8699d5df6380d2b3501903b5877aa4
Zahraa-06 <zahraasuhail2006@gmail.com>
2025-08-04 14:55:31 +0300

    Part 7: Initial MVP without CSS

--- models/db.js
+++ models/db.js
@@ -4,3 +4,5 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URI)
 
 module.exports = mongoose.connection
+
+//authenticate users for verified, sightings for specific user, admin can only edit species
